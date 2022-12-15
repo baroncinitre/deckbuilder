e5913f7dc94366bbd20c91c0abb25db46e9ccc59
baroncinitre <baroncinitre@gmail.com>
2022-12-15 01:56:47 -0500

    Add from file + pretty swap print

--- deckBuilder.py
+++ deckBuilder.py
@@ -1,4 +1,7 @@
 import sf_price_fetcher
+import copy
+import os
+from pprint import pprint
 from mtgtools.MtgDB import MtgDB
 from mtgtools.PCardList import PCardList
 from mtgtools.PSetList import PSetList
@@ -17,15 +20,42 @@ class deckBuilder:
         deck.name = deck_name
         setattr(db.root, deck_var_name, deck)
         db.commit()
+    
+    @staticmethod
+    def duplicate_deck(db, old_deck_var_name, new_deck_var_name, new_deck_name=''):
+        new_deck = copy.deepcopy(getattr(db.root, old_deck_var_name))
+        if new_deck_name: 
+            new_deck.name = new_deck_name
+        else:
+            new_deck.name = new_deck_var_name
+        setattr(db.root, new_deck_var_name, new_deck)
+        db.commit()
+
+    @staticmethod
+    def print_stored_pcardlists(db):
+        db_vars = db.root._root.data.keys()
+        pprint(db_vars)
 
     def print_decklist(self):
         print(self.deck.deck_str())
+    
+    def print_ordered_decklist(self):
+        for (i, item) in enumerate(self.deck.cards, start=0):
+            print(f"{i}: {item}")
 
     def save(self):
         self.db.commit()
 
     def remove_at_index(self, index, commit=True):
-        self.deck.pop(index)
+        if type(index) == list:
+            # This sorts and flips, removing from highest index to lowest
+            # this SHOULD always mean indices stay the same. TODO: verify
+            indices = sorted(index, reverse=True)
+            for i in indices:
+                self.deck.pop(i)
+        else:
+            self.deck.pop(index)
+
         self.save()
 
     def remove_card(self, card_name, commit=True, strict=True, deck=None):
@@ -44,6 +74,19 @@ class deckBuilder:
         except IndexError:
             print("No match in deck - check the name is spelled correctly (case insensitive)")
             return 1
+
+    # WARNING: finnicky for some reason
+    def add_from_file(self, fp, strict=False):
+        f = open(fp, "r")
+        data = f.read()
+        cards_to_add = data.split("\n")
+        failed_additions = []
+        for card in cards_to_add:
+            result = self.add_card(card, strict=strict)
+            if result: failed_additions.append(card)
+        print("\nFailed cards:")
+        print(failed_additions)
+
 
     def add_card(self, card_name, commit=True, strict=True, deck=None):
         # Takes the first result of the search. Name must match exactly if strict is True
@@ -66,29 +109,36 @@ class deckBuilder:
         self.remove_card(card_out, strict=out_strict, commit=out_commit)
         self.add_card(card_in, strict=in_strict, commit=in_commit)
 
-    def plan_swap(self, card_out, card_in, commit=True):
+    def plan_swap(self, card_out, card_in, strict=True):
+        #TODO make this less ugly
         out_list_name = f"{self.deck_var_name}_swap_out"
         in_list_name = f"{self.deck_var_name}_swap_in"
+
         try:
             out_list = getattr(self.db.root, out_list_name)
             in_list = getattr(self.db.root, in_list_name)
         except:
             self.create_deck(self.db, out_list_name, out_list_name)
             self.create_deck(self.db, in_list_name, in_list_name)
+            # gross
             out_list = getattr(self.db.root, out_list_name)
             in_list = getattr(self.db.root, in_list_name)
-        if self.add_card(card_out, deck=out_list):
+
+        if self.add_card(card_out, deck=out_list, strict=strict):
             print("Error setting out card, please try again")
             return
-        if self.add_card(card_in, deck=in_list):
+        if self.add_card(card_in, deck=in_list, strict=strict):
             print("Error setting in card, please try again")
             self.remove_card(card_out, deck=out_list)
             return
 
     def execute_swaps(self):
+        #TODO implement
         pass
 
     def print_planned_swaps(self):
+        #TODO maybe make prettier?
+        print()
         out_list_name = f"{self.deck_var_name}_swap_out"
         in_list_name = f"{self.deck_var_name}_swap_in"
         try:
@@ -98,7 +148,15 @@ class deckBuilder:
             print("No swaps planned")
             return
         
-        out_cards = out_plist.cards
-        in_cards = in_plist.cards
+        out_cards = [card.name for card in out_plist.cards]
+        in_cards = [card.name for card in in_plist.cards]
+        max_len = len(max(out_cards, key = len))
+        max_len_in = len(max(in_cards, key = len))
+        print(f"{'Swapping out:':{max_len}}\tReplacing with:")
+        print(f"="*(max_len+4+max_len_in))
         for i in range(len(out_cards)):
-            print(out_cards[i].name + '\t ' + in_cards[i].name)
+            out_card = out_cards[i]
+            in_card = in_cards[i]
+            formatted_swap = f'{out_card:{max_len}}\t{in_card}'
+            print(formatted_swap)
+        print()
--- main.py
+++ main.py
@@ -4,11 +4,20 @@ from mtgtools.MtgDB import MtgDB
 from mtgtools.PCardList import PCardList
 from mtgtools.PSetList import PersistentList
 
-# TODO: currently only configured for EDH, check if functions work with duplicate cards
+#TODO: repeat card checks for addition (ignore basic lands) and all swaps
+#TODO: currently only focusing on EDH, check if functions work with duplicate cards (maybe. non-EDH is lame)
+#TODO: implement swap execute
+#TODO: indexed removal with cards displayed sorted by type (land, creature, etc)
+#TODO: active UI in console (have enterable commands like remove to trigger removal, etc)
+#TODO: removal console UI
+#TODO: better addition functionality through console
+#TODO: exporting (export directly to apps via API?)
+
+# Pipe dreams: Flask app, GUI
 
 def main():
     # Current db is 'test_db.fs'
-    builder = deckBuilder('test_db.fs', 'testdeck')
+    builder = deckBuilder('test_db.fs', 'mmcurrent')
     builder.print_planned_swaps()
 
 if __name__ == "__main__":
